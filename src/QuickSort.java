414917c63be9b01abf088cd89ed5e69f6396af43
delahiri <debaniklahiri@gmail.com>
2015-09-25 20:58:36 -0500

    first commit

---
+++ src/QuickSort.java
@@ -0,0 +1,45 @@
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int a[] = {2,2,3,7,4,9,1,7,4,9,0,3};
+		quickSort(a, 0, a.length-1);
+		for(int n:a)
+			System.out.println(n);
+
+	}
+	
+	
+	static int partition(int arr[], int left, int right)
+	{
+	      int i = left, j = right;
+	      int tmp;
+	      int pivot = arr[(left + right) / 2];
+	     
+	      while (i <= j) {
+	            while (arr[i] < pivot)
+	                  i++;
+	            while (arr[j] > pivot)
+	                  j--;
+	            if (i <= j) {
+	                  tmp = arr[i];
+	                  arr[i] = arr[j];
+	                  arr[j] = tmp;
+	                  i++;
+	                  j--;
+	            }
+	      };
+	     
+	      return i;
+	}
+	 
+	static void quickSort(int arr[], int left, int right) {
+	      int index = partition(arr, left, right);
+	      if (left < index - 1)
+	            quickSort(arr, left, index - 1);
+	      if (index < right)
+	            quickSort(arr, index, right);
+	}
+
+
+}
